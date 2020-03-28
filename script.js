9fb4e063f161a0c6d0875011965b40db3cafebf6
Ali Shaukat <alishaukat.giki@gmail.com>
2020-03-28 22:23:40 +0500

    Basic syntax - Conditions

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-alert("Hello, World");
-alert("Hello, GIKI");
-alert("Hello, CS463");
+var x = 10;
+var y = 20;
+var big;
+
+if(x > y){
+    big = x;
+}else{
+    big = y;
+}
+
+big = x > y ? x : y;
+
+// condition?true:false;
+
+console.log(big);
+
+var x = 15;
+x += 10;
+
+if(x > 20){
+    console.log("x is greater than 20");
+}else if(x > 10){
+    console.log("x is between 10 and 20");
+}else{
+    console.log("x is less than 20");
+}
